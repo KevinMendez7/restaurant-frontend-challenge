bf0afca795076523dd3010272f02ecf693613874
= <=>
2021-07-22 02:53:56 -0600

    find routers function created to return map paths from routes

---
+++ test/utils/findRoutes.js
@@ -0,0 +1,9 @@
+const findRoutes = routes => {
+    return routes.reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+      }, {});
+};
+
+export default findRoutes
